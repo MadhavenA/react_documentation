4d22d07806115bb32f19958cc2ea14f8330e703a
Madhaven .A <158140160+Madhaven17@users.noreply.github.com>
2024-03-13 12:23:11 +0000

    adding the react documentaion files

--- react-documentation-related_examples/src/App.js
+++ react-documentation-related_examples/src/App.js
@@ -1,31 +1,30 @@
-import './App.css';
-import YourFirstComponentInJsx from './Describing_UI/YourFirstComponent/YourFirstComponentInJsx';
-import YourFirstComponentInJs from './Describing_UI/YourFirstComponent/YourFirstComponentInJs';
-import ImportingAndExportingComponents from './Describing_UI/ImportAndExportTheComponents/ImportingAndExportingComponents';
-import WritingMarkUpWithJsx from './Describing_UI/WritingMarkUpWithJsx/WritingMarkUpWithJsx';
-import JavaScriptinJsxWithCurlyBraces from './Describing_UI/JavaScriptinJsxWithCurlyBraces/JavaScriptinJsxWithCurlyBraces';
-import PassingPropsToAComponent  from './Describing_UI/PassingPropsToAComponent/PassingPropsToAComponent';
-import ConditionalRendering  from './Describing_UI/ConditionalRendering/ConditionalRendering';
-import RenderingList  from './Describing_UI/RenderingList/RenderingList';
-import KeepingComponentsPure  from './Describing_UI/KeepingComponentsPure/KeepingComponentsPure';
-import KeepingComponentsInPure  from './Describing_UI/KeepingComponentsPure/KeepingComponentsInPure';
-import Father from './Describing_UI/UIAsTree/Father';
-import FatherFamily from './Describing_UI/UIAsTree/JsFamily/FatherFamily';
-import EventHandler from './AddingInterActivity/RespondingToEvents/EventHandler';
-import UseStateUsing from './AddingInterActivity/StateAComponentMemory/UseStateUsing';
-import RenderAndCommit from './AddingInterActivity/RenderAndCommit/RenderAndCommit';
-import StateAsASnapShort from './AddingInterActivity/StateAsASnapShort/StateAsASnapShort';
-import QueueingASeriesOfStateUpdates from './AddingInterActivity/QueueingASeriesOfStateUpdates/QueueingASeriesOfStateUpdates';
-import UpdateObjectInState from './AddingInterActivity/UpdateObjectInState/UpdateObjectInState';
+import "./App.css";
+import YourFirstComponentInJsx from "./Describing_UI/YourFirstComponent/YourFirstComponentInJsx";
+import YourFirstComponentInJs from "./Describing_UI/YourFirstComponent/YourFirstComponentInJs";
+import ImportingAndExportingComponents from "./Describing_UI/ImportAndExportTheComponents/ImportingAndExportingComponents";
+import WritingMarkUpWithJsx from "./Describing_UI/WritingMarkUpWithJsx/WritingMarkUpWithJsx";
+import JavaScriptinJsxWithCurlyBraces from "./Describing_UI/JavaScriptinJsxWithCurlyBraces/JavaScriptinJsxWithCurlyBraces";
+import PassingPropsToAComponent from "./Describing_UI/PassingPropsToAComponent/PassingPropsToAComponent";
+import ConditionalRendering from "./Describing_UI/ConditionalRendering/ConditionalRendering";
+import RenderingList from "./Describing_UI/RenderingList/RenderingList";
+import KeepingComponentsPure from "./Describing_UI/KeepingComponentsPure/KeepingComponentsPure";
+import KeepingComponentsInPure from "./Describing_UI/KeepingComponentsPure/KeepingComponentsInPure";
+import Father from "./Describing_UI/UIAsTree/Father";
+import FatherFamily from "./Describing_UI/UIAsTree/JsFamily/FatherFamily";
+import EventHandler from "./AddingInterActivity/RespondingToEvents/EventHandler";
+import UseStateUsing from "./AddingInterActivity/StateAComponentMemory/UseStateUsing";
+import RenderAndCommit from "./AddingInterActivity/RenderAndCommit/RenderAndCommit";
+import StateAsASnapShort from "./AddingInterActivity/StateAsASnapShort/StateAsASnapShort";
+import QueueingASeriesOfStateUpdates from "./AddingInterActivity/QueueingASeriesOfStateUpdates/QueueingASeriesOfStateUpdates";
+import UpdateObjectInState from "./AddingInterActivity/UpdateObjectInState/UpdateObjectInState";
 
 function App() {
   return (
-  <>
-    <div className="App">
-
-        <UpdateObjectInState/>
-    </div>
-  </>
+    <>
+      <div className="App">
+        <UpdateObjectInState />
+      </div>
+    </>
   );
 }
 
