ab5de7894971c40ac1b7408b7663226655fcbbcd
madhavenmaddy17 <madhavenmaddy17@gmail.com>
2024-08-29 07:33:49 +0000

    changes and project must be run in the idx tool .

--- react-documentation-related_examples/src/App.js
+++ react-documentation-related_examples/src/App.js
@@ -21,11 +21,12 @@ import UpdateTheObjectByUsingState from "./AddingInterActivity/UpdatingArrayInSt
 import UsingImmerAddingListOfFruits from "./AddingInterActivity/UpdatingArrayInState/UsingImmerAddingListOfFruits";
 import ArrayInClassComponent from "./AddingInterActivity/UpdatingArrayInState/ArrayInClassComponent";
 import ReactingWithInputState from "./ManagingState/ReactingWithInputState/ReactingWithInputState";
+import ChoosingTheStateStructure from "./ManagingState/ReactingWithInputState/ChoosingTheStateStructure/ChoosingTheStateStructure";
 function App() {
   return (
     <>
       <div className="App">
-        <ReactingWithInputState />
+        <ChoosingTheStateStructure />
       </div>
     </>
   );
---
+++ react-documentation-related_examples/src/ManagingState/ReactingWithInputState/ChoosingTheStateStructure/ChoosingTheStateStructure.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default class ChoosingTheStateStructure extends React.Component {
+
+    render() {
+        return(
+            <h1>choosing the state structure</h1>
+        );
+    }
+}
